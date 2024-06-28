206ab770e25a9ac70b28ad190c94660c3eb09714
TanKaka123 <hongtan1422002@gmail.com>
2024-06-29 02:18:21 +0700

    config index project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
+import VideoList from "./pages/VideoList";
+import PersonalizedInformation from "./pages/PersonalizedInformation";
+import GenerateStory from "./pages/GenerateStory";
+import HistoryTeller from "./pages/HistoryTeller";
+import { useEffect, useState } from "react";
+
+function App() {
+
+  return (
+    <Routes>
+
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#AF5E27",
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
