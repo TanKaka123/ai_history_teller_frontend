cc14c6d36943aa668d0fd8e19048f11aa518fdf7
TanKaka123 <hongtan1422002@gmail.com>
2024-06-29 03:14:38 +0700

    create routing and require personalized infromation

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,35 @@ import HistoryTeller from "./pages/HistoryTeller";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [isChecking, setIsChecking] = useState(true);
 
+  useEffect(() => {
+    const isProvidePersonalizedInformation = localStorage.getItem(
+      "personalizedInformation"
+    );
+    if (isProvidePersonalizedInformation) {
+      setIsAuthorized(true);
+    }
+    setIsChecking(false);
+  }, []);
+
+  if (isChecking) {
+    return null;
+  }
   return (
     <Routes>
-
+      {isAuthorized ? (
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="stories" element={<VideoList />} />
+          <Route path="stories/:id" element={<VideoList />} />
+          <Route path="generate-story" element={<GenerateStory />} />
+          <Route path="history-teller/:id" element={<HistoryTeller />} />
+        </Route>
+      ) : (
+        <Route path="*" element={<PersonalizedInformation />} />
+      )}
     </Routes>
   );
 }
