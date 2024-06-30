625d651273eb833ef637894798184818c0e5ddce
TanKaka123 <hongtan1422002@gmail.com>
2024-06-30 07:56:07 +0700

    change style item

--- src/components/generateStory/HistoricalEventItem.tsx
+++ src/components/generateStory/HistoricalEventItem.tsx
@@ -16,22 +16,22 @@ export const HistoricalEventItem = ({
     <Row
       className={`${
         isSelected ? "bg-blue-50 border-primary" : "border-gray-300"
-      } w-full hover:bg-blue-50 border hover:border-primary  px-4 py-4 rounded-md cursor-pointer`}
+      } w-full flex flex-nowrap hover:bg-blue-50 border hover:border-primary  px-4 py-4 rounded-md cursor-pointer`}
       onClick={() => {
         if (onClick) onClick();
       }}
     >
-      <div className="w-40 ">
+      <div className="w-40">
         <Text className="text-lg font-bold text-primary">
           {event.timePeriod}
         </Text>
       </div>
       <div className="flex-1 space-y-2">
         <Text className="text-base font-bold text-primary">
-          {event.mainCharacter} {event.eventName}
+          {event.mainCharacter} - {event.eventName}
         </Text>
         <br />
-        <Text ellipsis={true} className="text-base text-gray-500">
+        <Text ellipsis={true} className="text-base text-gray-500 text-wrap line-clamp-1">
           {event.description}
         </Text>
       </div>
