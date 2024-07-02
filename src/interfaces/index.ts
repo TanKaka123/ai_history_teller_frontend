f40d46e12cb96e6f3524728cd7372a6e336abde0
TanKaka123 <hongtan1422002@gmail.com>
2024-07-02 12:39:06 +0700

    update interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,23 +1,30 @@
+export interface ISummary {
+  context: string,
+  historical_significance: string,
+  main_happenings: string,
+  result: string
+}
+
+export interface IQuestionaire {
+  question: string,
+  options: string[],
+  answer: number
+}
+
+export interface IStoryContent {
+  text: string;
+  voice_url: string;
+  fpt_url: string;
+  duration: number;
+  end_time: number;
+  start_time: number;
+  img_url: string,
+}
+
+
 export interface IHistoryTeller {
-  content: {
-    text: string,
-    voice_url: string,
-    fpt_url: string,
-    img_url: string,
-    duration: number,
-    start_time: number,
-    end_time: number
-  }[],
+  content: IStoryContent[],
   imgs: string[],
-  questionaire: {
-    question: string,
-    options: string[],
-    answer: number
-  }[],
-  summary: {
-    context: string,
-    historical_significance: string,
-    main_happenings: string,
-    result: string
-  }
+  questionaire: IQuestionaire[],
+  summary: ISummary
 }
