b2299aed3fcd25d3156de9a9a9b9aa80a0564b2f
TanKaka123 <hongtan1422002@gmail.com>
2024-07-02 12:39:19 +0700

    update bad case

--- src/pages/HistoryTeller.tsx
+++ src/pages/HistoryTeller.tsx
@@ -3,17 +3,25 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useStoryTeller } from "../hook/useStoryTeller";
 import { HistoricalVideo } from "../components/storyTeller/HistoricalVideo";
+import { HISTORICAL_EVENT } from "../constants/historicalEvents";
 
 const { Text } = Typography;
 const HistoryTeller = React.memo(() => {
   const { id } = useParams();
-
   const { dataHistory } = useStoryTeller(id!);
-  console.log({ dataHistory });
+
+  const selectedHistoricalEvent = React.useMemo(() => {
+    if (!id) return undefined;
+    return HISTORICAL_EVENT.find(
+      (event) => event.eventName.toLowerCase() == id.toLowerCase()
+    );
+  }, [id]);
   return (
     <React.Fragment>
-      {dataHistory ? (
-        <HistoricalVideo dataHistory={dataHistory} />
+      {!selectedHistoricalEvent ? (
+        <Text>Không tìm thấy sự kiện lịch sử</Text>
+      ) : dataHistory ? (
+        <HistoricalVideo dataHistory={dataHistory} historicalEvent={selectedHistoricalEvent}/>
       ) : (
         <div className="w-full h-screen  flex justify-center items-center flex-col">
           <Spin size="large"></Spin>
