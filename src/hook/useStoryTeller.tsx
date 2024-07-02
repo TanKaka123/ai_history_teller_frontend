c41d4422a2f770854f519d2a797a4382b6a7131c
TanKaka123 <hongtan1422002@gmail.com>
2024-07-02 12:47:12 +0700

    get history story

--- src/hook/useStoryTeller.tsx
+++ src/hook/useStoryTeller.tsx
@@ -4,11 +4,34 @@ import { MOCK_DATA } from "../constants/mockData";
 
 export const useStoryTeller = (eventName: string) => {
   const [dataHistory, setDataHistory] = React.useState<IHistoryTeller>();
-
+  const personalizedInformation = JSON.parse(
+    localStorage.getItem("personalizedInformation") ?? "{}"
+  );
   React.useEffect(() => {
-    setDataHistory(MOCK_DATA);
+    // Fetch data from API endpoint
+    fetch(process.env.REACT_APP_API_HISTORY_ENDPOINT, {
+      body: {
+        ...personalizedInformation,
+        eventName
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Assuming data is successfully fetched and parsed
+        setDataHistory(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        // Fallback to mock data if fetching fails
+        setDataHistory(MOCK_DATA);
+      });
   }, []);
-  
+
   return {
     dataHistory,
   };
